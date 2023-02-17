8340881765a3a060a9bc2f1361fd1a1bbe0b2716
greeneley <anhchangtoanhoc97@gmail.com>
2023-02-17 22:57:00 +0700

    lesson 2: initial files for our project

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+
+const app = express();
+
+// init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+//init db
+
+//init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello Factips";
+
+  return res.status(200).json({
+    message: "Welcome Fantipsjs",
+    metadata: strCompress.repeat(10000),
+  });
+});
+
+//handling error
+
+module.exports = app;
